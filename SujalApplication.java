5a0fec20417579049bb41e01791d2f02ff9636ca
sujalpal2004 <164747250+sujalpal2004@users.noreply.github.com>
2024-03-24 17:43:43 +0530

    SujalApplication.java

--- SujalApplication.java
+++ SujalApplication.java
@@ -4,40 +4,6 @@ public class SujalApplication {
 
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        /*System.out.println("Enter Your age: ");
-        int age = scanner.nextInt();
-        System.out.println("Your age is: "+age);*/
-        
-        //How to calculate total marks and percentage of the subjects.
-        
-/*        System.out.println("Enter your marks of Physics: ");
-        int Physics = scanner.nextInt();
-        
-         System.out.println("Enter your marks of Chemistry: ");
-        int Chemistry= scanner.nextInt();
-        
-        System.out.println("Enter your marks of Maths: ");
-        int Maths= scanner.nextInt();
-        
-        int TotalMarks = Physics + Chemistry + Maths;
-        System.out.println("Your TotalMarks is : "+TotalMarks);
-          float Percentage = (Physics + Chemistry + Maths/300)*100;
-        System.out.println("Your Percentage is : "+Percentage);*/
-        //using if , if-else conditions in statements in age wise discussions
-      /*  System.out.println("Enter your age : ");
-        int age = scanner.nextInt();
-        if(age>18)
-        {
-            System.out.println("You are an  adult now");
-        }
-        else if(age>12 && age<18){
-            System.out.println("You are a teenager now");
-        }
-        else
-        {
-        System.out.println("You are not an  adult");
-        }*/
-      
       //how to make an calculator in java
       System.out.println("Welcome to Basic Calculator!");
         System.out.println("Enter the first number:");
@@ -75,8 +41,9 @@ public class SujalApplication {
         }
 
         System.out.println("Result: " + result);
+        
      //how to make simple basic calculator using if , else statements
-    /* System.out.println("Enter your first num1 :  ");
+     System.out.println("Enter your first num1 :  ");
      double num1 = scanner.nextDouble();
      
      System.out.println("Enter your second num2 : ");
@@ -107,7 +74,7 @@ public class SujalApplication {
          System.out.println("Invalid Operations");
          return;
      }
-     System.out.println("Your  result is : "+Result);*/
+     System.out.println("Your  result is : "+Result);
     }
 }
       
